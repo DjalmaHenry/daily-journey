c7af87731f0720b9f8385eb7ca7b393f90bd0410
Djalma Henrique <contato@djalmahenry.com>
2022-07-23 17:34:31 -0300

    feat: add new assets and loading comonent

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { NativeBaseProvider } from "native-base";
 import { SignIn } from "./src/screens/SignIn";
+import { Loading } from "./src/components/Loading";
+import { THEME } from "./src/styles/theme";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
