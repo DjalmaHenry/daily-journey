6f9db84472acf710e04f3e17c2eef1abfec9096d
Djalma Henrique <contato@djalmahenry.com>
2022-07-24 12:01:12 -0300

    feat: add login screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
 import { SignIn } from "./src/screens/SignIn";
 import { Loading } from "./src/components/Loading";
 import { THEME } from "./src/styles/theme";
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,9 @@
-import { Center, Spinner } from 'native-base';
+import { Center, Spinner } from "native-base";
 
 export function Loading() {
   return (
-    <Center flex={1} bg='gray.700'>
-        <Spinner color='secundary.700' />
+    <Center flex={1} bg="gray.700">
+      <Spinner color="secundary.700" />
     </Center>
   );
 }
